23df7c1d77e45f9a8d9bc23fbd6b73b28f55a1b3
Charlie Arboleda <charliearboleda@Charlies-MacBook-Air.local>
2021-06-04 13:18:41 -0400

    update

--- db.js
+++ db.js
@@ -1,8 +1,8 @@
 const Data = require("pg").Data
 
 const Data = new Data({
-    user: "postgres",
-    password:"test1234567",
+    user: 'postgres',
+    password:'test1234567',
     host:"localhost",
     port:'5432',
     database:'moneymatters'
