f84aeaff6cf02609d83c5862ab44855eadec958b
Charlie Arboleda <charliearboleda@Charlies-MacBook-Air.local>
2021-06-08 18:43:09 -0400

    changes

--- index.js
+++ index.js
@@ -1,17 +1,17 @@
 const express = require('express')
 const app = express()
 const cors = require('cors')
-
+const PORT = process.env.PORT
 
 
 app.use(express.json())
 app.use(cors())
-app.set(PORT, (process.env.PORT || 3000))
+
 app.use('/auth', require('./routes/Auth'))
 
 app.use("/dashboard", require("./routes/Dashboard"))
 
 
-app.listen(3000, ()=>{
+app.listen(PORT, ()=>{
     console.log("Working in port ")
 })
