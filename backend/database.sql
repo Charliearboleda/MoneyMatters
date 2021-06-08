1e246be78289d47490ae5add9cf4e8a1f6a02e28
Charlie Arboleda <charliearboleda@Charlies-MacBook-Air.local>
2021-06-08 01:22:39 -0400

    update

--- backend/database.sql
+++ backend/database.sql
@@ -5,7 +5,7 @@ CREATE TABLE users (
     user_name VARCHAR(128) NOT NULL,
     user_email VARCHAR(128) NOT NULL,
     user_password VARCHAR(128) NOT NULL,
-    account_balance INTEGER()
+    account_balance INTEGER
 );
 
 INSERT INTO users (user_name, user_email, user_password) VALUES('charlie', 'charlieiarboleda@gmail.com', 'test123456');
