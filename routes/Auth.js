edeea679a5808ed7a7f167bf21a80e3ee49671cd
Charlie Arboleda <charliearboleda@Charlies-MacBook-Air.local>
2021-06-08 19:56:52 -0400

    updated

--- routes/Auth.js
+++ routes/Auth.js
@@ -9,7 +9,7 @@ router.post("/register", async (req, res) =>{
     try {
         const {user_name, user_email, user_password, account_balance} = req.body
         const user = await data.query("SELECT * FROM users WHERE user_email = $1", [
-            email
+            user_email
         ])
         // if(user.rows.length !== 0){
         //     return res.status(401).send("User Already Exist")
@@ -19,7 +19,7 @@ router.post("/register", async (req, res) =>{
     const salt = await bcrypt.genSalt(saltRound)
     const bcryptPassword = await bcrypt.hash(password, salt)
 
-    const newUser = await data.query("INSERT INTO users (user_name, user_email, user_password, account_balance) VALUES ($1, $2, $3, $4) RETURNING *",[name, email, bcryptPassword, account_balance])
+    const newUser = await data.query("INSERT INTO users (user_name, user_email, user_password, account_balance) VALUES ($1, $2, $3, $4) RETURNING *",[user_name, user_email, bcryptPassword, account_balance])
 
 
     const token = jwtGenerator(newUser.rows[0].user_id)
@@ -33,15 +33,15 @@ router.post("/register", async (req, res) =>{
 
 router.post("/login",validInfo, async(req, res)=> {
     try {
-        const {email, password} = req.body
+        const {user_email, user_password} = req.body
         const user = await data.query("SELECT * FROM users WHERE user_email = $1", [
-            email
+            user_email
         ])
         if(user.rows.length === 0){
         return  res.status(401).json("Email or Password is inCorrect")
         }
 
-        const validPassword = await bcrypt.compare(password, user.rows[0].user_password)
+        const validPassword = await bcrypt.compare(user_password, user.rows[0].user_password)
         if(!validPassword ){
             return res.status(401).json("email or password is incorrect")
         }
