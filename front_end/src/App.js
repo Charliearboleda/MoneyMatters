c88aee3369d17024d1215c6353cb06e7ab51b386
Charlie Arboleda <charliearboleda@Charlies-MacBook-Air.local>
2021-06-09 00:03:37 -0400

    we added forms for register and checked the state is working when we put values into the form

--- front_end/src/App.js
+++ front_end/src/App.js
@@ -7,14 +7,18 @@ import Login from "./components/login"
 import Register from "./components/register"
 function App() {
     const [isAuthenticated, setIsAuthenticated] = useState(false)
+    const setAuth = (boolean) => {
+        setIsAuthenticated(boolean)
+    }
+
   return (
     <Fragment>
     <Router>
     <div className="container">
     <Switch>
-    <Route exact path="/login" render={props => !isAuthenticated ? <Login {...props} /> : <Redirect to="/dashboard" />}/>
-    <Route exact path="/register" render={props => !isAuthenticated? <Register {...props} /> : <Redirect to="/login" />}/>
-    <Route exact path="/dashboard" render={props => isAuthenticated ? <Dashboard {...props} /> : <Redirect to="/login" />}/>
+    <Route exact path="/login" render={props => !isAuthenticated ? <Login {...props} setAuth={setAuth} /> : <Redirect to="/dashboard" />}/>
+    <Route exact path="/register" render={props => !isAuthenticated? <Register {...props} setAuth={setAuth} /> : <Redirect to="/login" />}/>
+    <Route exact path="/dashboard" render={props => isAuthenticated ? <Dashboard {...props} setAuth={setAuth} /> : <Redirect to="/login" />}/>
     </Switch>
     </div>
     </Router>
--- front_end/src/components/register.js
+++ front_end/src/components/register.js
@@ -1,9 +1,34 @@
-import React, { Fragment } from "react"
+import React, { Fragment, useState } from "react"
 
 const Register = () => {
+
+    const [inputs, setInputs] = useState({
+        userName: "",
+        email: "",
+        password: "",
+        accountBalance: ""
+    })
+
+    const { userName, email, password, accountBalance } = inputs
+
+  const onChange = e => {
+      setInputs({...inputs, [e.target.name] : e.target.value})
+  }
+
     return (
         <Fragment>
-        <h1>Register</h1>
+        <h1 className="text-center my-5">Register</h1>
+        <form>
+        <label htmlFor="userName">User Name </label>
+        <input type="text" name="userName" placeholder="name" className="form-control my-3" value={userName} onChange={e => onChange(e)}/>
+        <label htmlFor="email">Email </label>
+        <input type="email" name="email" placeholder="email" className="form-control my-3" value={email} onChange= {e => onChange(e)} />
+        <label htmlFor="password">Password </label>
+        <input type="password" name="password" placeholder="password" className="form-control my-3" value={password} onChange= {e => onChange(e)}/>
+        <label htmlFor="accountBalance">Account Balance $ </label>
+        <input type="text" name="accountBalance" placeholder="balance" className="form-control my-3" value={accountBalance} onChange= {e => onChange(e)} />
+        <button className="btn btn-success btn-block">Submit</button>
+        </form>
         </Fragment>
     )
 }
