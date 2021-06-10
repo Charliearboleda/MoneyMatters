02753a664558b4cc7debb5d5b0a624bc077911e8
Charlie Arboleda <charliearboleda@Charlies-MacBook-Air.local>
2021-06-10 19:51:34 -0400

    update backend

--- index.js
+++ index.js
@@ -2,10 +2,15 @@ const express = require('express')
 const app = express()
 const cors = require('cors')
 const PORT = process.env.PORT
+const corsOptions = {
+  origin: "http://localhost:3000",
+  optionsSuccessStatus:200
+}
+app.use(cors(corsOptions))
 require('dotenv').config()
 
 app.use(express.json())
-app.use(cors())
+
 
 app.use('/auth', require('./routes/Auth'))
 
