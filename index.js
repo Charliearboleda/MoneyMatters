bc51fdcddc5091ad224c6526fa2c2cbdb758f21d
Charlie Arboleda <charliearboleda@Charlies-Air.lan>
2021-09-14 17:50:28 -0400

    update

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const app = express()
 const cors = require('cors')
 const PORT = process.env.PORT
 const corsOptions = {
-  origin: "https://sharp-torvalds-d6526b.netlify.app/",
+  origin: "https://money-managment-nyc.herokuapp.com/",
   optionsSuccessStatus:200
 }
 app.use(cors(corsOptions))
